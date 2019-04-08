7a5f1ba6e23193f425cb5fe5bba092b90584fc6b
odan <odan3240@gmail.com>
2019-04-09 01:51:21 +0900

    add circle ci config

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,12 @@
 
 set -eu
 
+echo "setup assume role"
+sts=$(aws sts get-session-token)
+export AWS_ACCESS_KEY_ID=$(echo $sts | jq '.Credentials.AccessKeyId')
+export AWS_SECRET_ACCESS_KEY=$(echo $sts | jq '.Credentials.SecretAccessKey')
+export AWS_SESSION_TOKEN=$(echo $sts | jq '.Credentials.SessionToken')
+
 echo "copy to s3 bucket"
 aws s3 cp --recursive app/dist/ s3://odan.dev/
 
