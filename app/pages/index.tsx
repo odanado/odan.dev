a0d9d2d40e76496e8f949d8887fa4afd6b713aad
odan <odan3240@gmail.com>
2022-05-07 00:38:43 +0900

    feat(app): アイコンを追加

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
+
 import Header from "../components/Header";
+import AccountList from "../components/AccountList";
 
 const Home: NextPage = () => {
   return (
@@ -13,8 +16,25 @@ const Home: NextPage = () => {
 
       <Header />
 
-      <main>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <main className="bg-gray-100 h-screen w-screen">
+        <div className="md:container md:mx-auto px-4 w-full h-full flex items-center justify-center flex-col">
+          <Image
+            src="/icon.jpeg"
+            width={128}
+            height={128}
+            alt="My icon"
+            className="rounded-full"
+          />
+          <section className="w-full max-w-2xl border-solid border-2 border-blue-500 rounded p-4 mt-6">
+            <h2 className="font-bold text-2xl">Abount me</h2>
+            <p className="pt-2">
+              ソフトウェアエンジニアです。
+              <br />
+              Web 技術やブロックチェーンに興味があります。
+            </p>
+            <AccountList className="py-4" />
+          </section>
+        </div>
       </main>
     </div>
   );
