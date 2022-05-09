e4399a7785f7642e159a56469c23c64033a74175
odan <odan3240@gmail.com>
2022-05-10 01:54:48 +0900

    fix(app): 高さをいい感じに

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,13 +2,13 @@ import { FC } from "react";
 import Link from "next/link";
 
 const Header: FC = () => (
-  <div className="h-16 bg-blue-600  flex items-center justify-items-center w-full">
+  <header className="h-16 bg-blue-600  flex items-center justify-items-center w-full">
     <div className="text-slate-200 font-bold w-full text-center">
       <Link href="/">
         <a>odan.dev</a>
       </Link>
     </div>
-  </div>
+  </header>
 );
 
 export default Header;
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -7,10 +7,10 @@ import Icon from "../components/Icon";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="h-screen w-screen flex flex-col">
       <Header />
 
-      <main className="bg-gray-100 h-screen w-screen">
+      <main className="bg-gray-100 flex-grow">
         <div className="md:container md:mx-auto px-4 w-full h-full flex items-center justify-center flex-col">
           <Icon />
           <section className="w-full max-w-2xl border-solid border-2 border-blue-500 rounded p-4 mt-6">
