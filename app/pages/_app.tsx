e0a119e5f7fdd52c91f87c4d6769f2365b451b69
odan <odan3240@gmail.com>
2022-05-10 01:42:34 +0900

    fix(app): SEO をいい感じに

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        title="odan.dev"
+        description="ソフトウェアエンジニアです。"
+        openGraph={{
+          url: "https://odan.dev",
+          images: [{ url: "https://odan.dev/icon.jpeg" }],
+        }}
+        twitter={{ handle: "@odan3240" }}
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "/favicon.ico",
+          },
+        ]}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
