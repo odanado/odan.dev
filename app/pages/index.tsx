7ed1d4c50c9cd0a9bc7511149a3c3cb79ac72e5f
odan <odan3240@gmail.com>
2022-11-04 00:56:02 +0900

    たいぽしてた

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
         <div className="md:container md:mx-auto px-4 w-full h-full flex items-center justify-center flex-col">
           <Icon />
           <section className="w-full max-w-2xl border-solid border-2 border-blue-500 rounded p-4 mt-6">
-            <h2 className="font-bold text-2xl">Abount me</h2>
+            <h2 className="font-bold text-2xl">About me</h2>
             <p className="pt-2">
               ソフトウェアエンジニアです。
               <br />
