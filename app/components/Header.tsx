134b35b0afd98d9d8aae16a90c9239849bc7d130
odan <odan3240@gmail.com>
2023-05-21 21:46:12 +0900

    run npx @next/codemod@latest new-link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: FC = () => (
   <header className="h-16 bg-blue-600  flex items-center justify-items-center w-full">
     <div className="text-slate-200 font-bold w-full text-center">
       <Link href="/">
-        <a>odan.dev</a>
+        odan.dev
       </Link>
     </div>
   </header>
